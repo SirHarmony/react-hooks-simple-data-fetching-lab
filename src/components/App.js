885738e0b078f6c387d8e4ccd03b2180eb67114f
Harmony <harmonymukolwe@gmail.com>
2024-08-12 09:30:40 +0300

    Displays random dog image in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,15 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dogImg, setDogImg] = useState("");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((dog) => setDogImg(dog.message));
+  });
+
+  return dogImg ? <img src={dogImg} alt="A Random Dog" /> : <p>Loading...</p>;
+}
+
+export default App;
